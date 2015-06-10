69f163f715f6f247cc919c83e8883b5f8029eadf
Kyle Brown Aisho <kyleaisho@gmail.com>
2015-06-10 15:25:05 -0700

    Create create.sql
    Added the table sql definitions from the formal proposal

---
+++ create.sql
@@ -0,0 +1,66 @@
+
+CREATE TABLE Staff(
+ SIN CHAR(11),
+ Name CHAR(25),
+ primary key(SIN));
+
+
+CREATE TABLE Customer(
+ CID Integer,
+ Name CHAR(25),
+ primary key(CID));
+
+CREATE TABLE Customer_Service(
+CID Integer,
+Date Datetime,
+SIN CHAR(11),
+primary key(CID,Date),
+foreign key SIN references Staff(SIN));
+
+CREATE TABLE Recipes(
+ rName CHAR(25),
+ Ingredients char(100),
+ primary key(rName));
+
+
+CREATE TABLE Recipes_Created(
+rName CHAR(25),
+SIN CHAR(11),
+primary key(rName),
+foreign key SIN references Staff(SIN) 
+ON DELETE CASCADE);
+
+CREATE TABLE Order_Placed_Ordered(
+ID Integer,
+CID Integer,
+Date Datetime,
+mName CHAR(25),
+primary key(ID,CID),
+foreign key CID references Customer_Serves(CID),
+foreign key mName references Recipes_Created(rName));
+
+CREATE TABLE Recipe_To_Menu(
+rName Char(25),
+mName Char(25),
+primary key(rName, mName),
+foreign key rName references Recipes_Created(rName)
+ON DELETE CASCADE);
+
+CREATE TABLE Menu_Price(
+rName CHAR(25),
+Price Decimal,
+primary key(rName),
+foreign key rName references Recipes_Created(rName));
+
+CREATE TABLE Stock(
+sName CHAR(15),
+Quantity Integer,
+UnitPrice Decimal,
+primary key(sName));
+
+CREATE TABLE Requires(
+sName CHAR(15),
+mName CHAR(25),
+primary key(sName, mName),
+foreign key sName references Stock(sName) ,
+foreign key mName references Menu_Featured(mName));
