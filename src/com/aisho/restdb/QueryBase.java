d417ca5a321ea47c6457b8e8df51cf129f286879
Kelvin Leung <kuvius@github.com>
2015-06-11 20:19:26 -0700

    Added delete, update and select queries.

--- src/com/aisho/restdb/QueryBase.java
+++ src/com/aisho/restdb/QueryBase.java
@@ -7,8 +7,10 @@ import java.io.*;
 public class QueryBase {
 	
 	/**
+	 * A basic insert query.
+	 * 
 	 * @param con The connection object created to access the Oracle DB.
-	 * @param table The name of the table.
+	 * @param table The name of the table(s).
 	 * @param args The tuple to be inserted. This is an arbitrary length of objects separated by commas,
 	 * e.g. 2, "Server", 3.5
 	 */
@@ -17,13 +19,11 @@ public class QueryBase {
 		PreparedStatement ps;
 		try {			
 			ps = con.prepareStatement("INSERT INTO " + table + " VALUES ("+ args + ")");
-			
-			  ps.executeUpdate();
+			ps.executeUpdate();
 
-			  // commit work 
-			  con.commit();
-
-			  ps.close();
+			// commit work 
+			con.commit();
+			ps.close();
 			  
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -31,13 +31,118 @@ public class QueryBase {
 		    try {
 		
 		    	// undo the insert
+		    	System.out.println("Error found. Rolling back the insertion...");
 		    	con.rollback();	
 		    }
 		    
-		    catch (SQLException ex2) {		    
-		    	System.out.println("Message: " + ex2.getMessage());
+		    catch (SQLException e2) {		    
+		    	System.out.println("Message: " + e2.getMessage());
 		    	System.exit(-1);
 		    }
 		}
+	}
+	
+	/**
+	 * A basic delete query.
+	 * 
+	 * @param con The connection object created to access the Oracle DB.
+	 * @param table The name of the table(s).
+	 * @param whereClause The SQL used in the where clause in the form of a string. Do not insert
+	 * WHERE or semicolons.
+	 */	
+	protected void sqlDelete(Connection con, String table, String whereClause) {
+		
+		PreparedStatement ps;
+		try {			
+			ps = con.prepareStatement("DELETE FROM " + table + " WHERE "+ whereClause);
+			ps.executeUpdate();
+
+			// commit work 
+			con.commit();
+			ps.close();
+			  
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    try {
+		
+		    	// undo the delete
+		    	System.out.println("Error found. Rolling back the deletion...");
+		    	con.rollback();	
+		    }
+		    
+		    catch (SQLException e2) {		    
+		    	System.out.println("Message: " + e2.getMessage());
+		    	System.exit(-1);
+		    }
+		}
+	}
+	
+	/**
+	 * A basic update query.
+	 * 
+	 * @param con The connection object created to access the Oracle DB.
+	 * @param table The name of the table(s).
+	 * @param setClause The SQL used in the set clause in the form of a string. Do not insert
+	 * SET.
+	 * @param whereClause The SQL used in the where clause in the form of a string. Do not insert
+	 * WHERE or semicolons.
+	 */	
+	protected void sqlUpdate(Connection con, String table, String setClause, String whereClause) {
+		
+		PreparedStatement ps;
+		try {			
+			ps = con.prepareStatement("UPDATE " + table + " SET " + setClause + " WHERE "+ whereClause);
+			ps.executeUpdate();
+
+			// commit work 
+			con.commit();
+			ps.close();
+			  
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    try {
+		
+		    	// undo the update
+		    	System.out.println("Error found. Rolling back the update...");
+		    	con.rollback();	
+		    }
+		    
+		    catch (SQLException e2) {		    
+		    	System.out.println("Message: " + e2.getMessage());
+		    	System.exit(-1);
+		    }
+		}
+	}
+	
+	/**
+	 * A basic select query.
+	 * 
+	 * @param con The connection object created to access the Oracle DB.
+	 * @param selectClause The select part of the query. Do not include the SELECT, e.g. "*", "x=3"
+	 * @param table The name of the table(s).
+	 * @param clause The part of the query that comes after the "FROM table " clause. Since any number
+	 * of clauses can be omitted, you will have to type out the rest of the query manually. e.g. "WHERE x=3",
+	 * "GROUP BY y", "WHERE x=3 GROUP BY y HAVING z=2". Do not include semicolons.
+	 * @return result The result of the Select query in the form of a ResultSet object. Check the Javadoc for
+	 * ResultSet for getter methods and other information.
+	 */	
+	protected ResultSet sqlSelect(Connection con, String selectClause, String table, String clause) {
+		
+		PreparedStatement ps;
+		ResultSet result = null;
+		try {			
+			ps = con.prepareStatement("SELECT " + selectClause + " FROM " + table + " " + clause);
+			result = ps.executeQuery();
+			  
+			ps.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
