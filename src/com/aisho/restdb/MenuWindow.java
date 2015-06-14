a28f2aa8f7def443f359f3fa3fc77d04ba7f520d
Kyle Brown Aisho <kyleaisho@gmail.com>
2015-06-14 13:52:13 -0700

    Added recipes and menu windows

---
+++ src/com/aisho/restdb/MenuWindow.java
@@ -0,0 +1,46 @@
+package com.aisho.restdb;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MenuWindow extends JFrame {
+
+	private JPanel contentPane;
+	
+	private static final int WIDTH = 600;
+	private static final int HEIGHT = 400;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuWindow frame = new MenuWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, WIDTH, HEIGHT);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
