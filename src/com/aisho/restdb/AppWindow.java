7794cba3d5eec85cea7116a3d870d921dae1bac8
Kyle Brown Aisho <kyleaisho@gmail.com>
2015-06-14 16:36:27 -0700

    Switched to card layout

--- src/com/aisho/restdb/AppWindow.java
+++ src/com/aisho/restdb/AppWindow.java
@@ -2,6 +2,7 @@ package com.aisho.restdb;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Panel;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,14 +18,29 @@ import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
+import java.awt.Font;
+import java.awt.CardLayout;
+
 public class AppWindow extends JFrame {
 	
 	private static final int HEIGHT = 600;
 	private static final int WIDTH = 800;
 	private static final String TITLE = "Restaurant DB";
+	
+	private static final String RECIPE = "Recipe";
+	private static final String STAFF = "Staff";
+	private static final String MENU = "Menu";
+	private static final String STOCK = "Stock";
+	
 	private JTextField txtTableNumber;
 	private JTextField txtCustomerId;
 	private JTextField txtMenuItem;
+	
+	private static int numOfCards = 0;
+	private JPanel recipeCard;
+	private JPanel menuCard;
+	private JPanel staffCard;
+	private JPanel stockCard;
 	
 
 	/**
@@ -52,6 +68,14 @@ public class AppWindow extends JFrame {
 		setBounds(100, 100, WIDTH, HEIGHT);
 		getContentPane().setLayout(null);
 		
+
+		final JPanel mainPanel = new JPanel();
+		mainPanel.setBounds(10, 116, 764, 434);
+		getContentPane().add(mainPanel);
+		mainPanel.setLayout(new CardLayout(0, 0));
+		
+		setUpCards(mainPanel);
+		
 		JRadioButton rdbtnAdministrator = new JRadioButton("Administrator");
 		rdbtnAdministrator.setBounds(453, 6, 141, 23);
 		getContentPane().add(rdbtnAdministrator);
@@ -71,30 +95,33 @@ public class AppWindow extends JFrame {
 		
 		txtCustomerId = new JTextField();
 		txtCustomerId.setText("Customer ID");
-		txtCustomerId.setBounds(17, 70, 134, 28);
+		txtCustomerId.setBounds(161, 34, 134, 28);
 		getContentPane().add(txtCustomerId);
 		txtCustomerId.setColumns(10);
 		
 		txtMenuItem = new JTextField();
 		txtMenuItem.setText("Menu Item");
-		txtMenuItem.setBounds(17, 110, 134, 28);
+		txtMenuItem.setBounds(305, 34, 134, 28);
 		getContentPane().add(txtMenuItem);
 		txtMenuItem.setColumns(10);
 		
+		
+		//Create buttons
 		JButton btnRecipes = new JButton("Recipes");
-		btnRecipes.setBounds(477, 56, 117, 29);
+		btnRecipes.setBounds(453, 36, 117, 29);
 		getContentPane().add(btnRecipes);
 		btnRecipes.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Recipe Click");
+				recipeCard.setVisible(true);
 			}
 			
 		});
 		
 		final JButton btnStaff = new JButton("Staff");
-		btnStaff.setBounds(477, 213, 117, 29);
+		btnStaff.setBounds(580, 76, 117, 29);
 		getContentPane().add(btnStaff);
 		btnStaff.setVisible(false);
 		btnStaff.addActionListener(new ActionListener() {
@@ -107,7 +134,7 @@ public class AppWindow extends JFrame {
 		});
 		
 		JButton btnCustomers = new JButton("Customers");
-		btnCustomers.setBounds(477, 110, 117, 29);
+		btnCustomers.setBounds(580, 36, 117, 29);
 		getContentPane().add(btnCustomers);
 		btnCustomers.addActionListener(new ActionListener() {
 
@@ -119,11 +146,11 @@ public class AppWindow extends JFrame {
 		});
 		
 		JButton btnStock = new JButton("Stock");
-		btnStock.setBounds(477, 165, 117, 29);
+		btnStock.setBounds(453, 76, 117, 29);
 		getContentPane().add(btnStock);
 		
 		JButton btnOrder = new JButton("Order");
-		btnOrder.setBounds(17, 154, 117, 29);
+		btnOrder.setBounds(17, 76, 117, 29);
 		getContentPane().add(btnOrder);
 		
 		btnOrder.addActionListener(new ActionListener() {
@@ -154,5 +181,38 @@ public class AppWindow extends JFrame {
 			}
 			
 		});
+	}
+	
+	/**
+	 * Set up the cards for a jpanel
+	 * @param mainPanel
+	 */
+	private void setUpCards(JPanel mainPanel) {
+		
+		recipeCard = new JPanel();
+		recipeCard.setVisible(false);
+		numOfCards++;
+		
+		menuCard = new JPanel();
+		menuCard.setVisible(false);
+		numOfCards++;
+		
+		staffCard = new JPanel();
+		staffCard.setVisible(false);
+		numOfCards++;
+		
+		stockCard = new JPanel();
+		stockCard.setVisible(false);
+		numOfCards++;
+		
+		mainPanel.add(recipeCard, RECIPE);
+		
+		JLabel lblRecipe = new JLabel(RECIPE);
+		recipeCard.add(lblRecipe);
+		mainPanel.add(menuCard, MENU);
+		mainPanel.add(staffCard, STAFF);
+		mainPanel.add(stockCard, STOCK);
+			
+		
 	}
 }
