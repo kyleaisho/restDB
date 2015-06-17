daadab3cdfb17fd31435b186e3ae1e007aa9b4a2
Ahria <ahrshia@hotmail.com>
2015-06-16 18:51:57 -0700

    Fixed Stock

--- src/com/aisho/restdb/Main.java
+++ src/com/aisho/restdb/Main.java
@@ -3,7 +3,7 @@ package com.aisho.restdb;
 public class Main {
 
 	public static void main(String[] args) {
-		OrderQueries.enterOrder(17,"'pizza'");
+		OrderQueries.enterOrder(3,"'Riviera Bouillabaisse'");
 		
 		
 		//Test your queries here. Leave the below area commented out!
--- src/com/aisho/restdb/StockQueries.java
+++ src/com/aisho/restdb/StockQueries.java
@@ -9,16 +9,22 @@ public class StockQueries extends QueryBase {
 	 * @param qty The quantity of the new stock item
 	 * @param unitPrice The unit price of the new stock item
 	 */
+	// Merge Two recipe tables
 	protected static void addStock(String sName, int qty, float unitPrice) {
-		ResultSet rs=sqlSelect("sName,Quantity","Stock","");
+		ResultSet rs=sqlSelect("sName,Quantity","Stock".trim(),"".trim());
 		boolean haveIt = false;
 		int howMuch = 0;
 		try {
 			while(rs.next()){
 				System.out.println(sName);
-				System.out.println("'".concat(rs.getString(1)).trim().concat("'"));
-				System.out.println("'".concat(rs.getString(1)).trim().concat("'").compareTo(sName));
-				if("'".concat(rs.getString(1)).trim().concat("'").compareTo(sName)==0){
+				System.out.println("'".concat(rs.getString(1).trim().concat("'")));
+				String x = "'".concat(rs.getString(1).trim().concat("'"));
+				//System.out.println("'".concat(rs.getString(1)).trim().concat("'"));
+				//System.out.println("'".concat(rs.getString(1)).trim().concat("'").compareTo(sName));
+				//System.out.println(sName);
+				//System.out.println(sqlStringify(sName));
+				System.out.println(sName.equals(x));
+				if(sName.equals(x)){
 					haveIt=true;
 					howMuch=rs.getInt(2);
 					break;
@@ -43,8 +49,34 @@ public class StockQueries extends QueryBase {
 	 * @param qty The new quantity of the stock item
 	 * @param unitPrice The new unit price of the stock item
 	 */
-	protected static void modifyStock(String item) {
-		sqlUpdate("Stock", "Stock.Quantity = Stock.Quantity - 1 ","Sname = "+ item);
+	protected static boolean modifyStock(ResultSet rs2) {
+		try {
+			while(rs2.next()){
+				System.out.println("In the first Loop");
+				ResultSet RA = sqlSelect("Quantity","Stock", "where sName = "+"'"+rs2.getString(1).trim()+"'");
+				RA.first();
+				System.out.println(RA.getInt(1));
+				if(RA.getInt(1)== 0){
+					System.out.println("IN");
+					return false;
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			printResultSet(rs2);
+			rs2.first();
+			rs2.beforeFirst();
+			while(rs2.next()){
+		sqlUpdate("Stock", "Stock.Quantity = Stock.Quantity - 1 ","Sname = "+"'" + rs2.getString(1).trim() + "'");
+			}
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
 		
 		//Can be used to add or subtract from the current stock
 		//unitPrice might be unnecessary if we never need to modify it? 
@@ -56,4 +88,5 @@ public class StockQueries extends QueryBase {
 		ResultSet rs = sqlSelect("*", "Recipe", "");
 		printResultSet(rs);
 	}
+
 }
